b63ca378dbd14be444e18caf770692b3536de40d
Fagun Bhavsar <fagun@newfang.io>
2022-12-07 13:18:15 +0530

    Page objects are modified for both Homepage and Login page

--- pages/homepage.ts
+++ pages/homepage.ts
@@ -1,11 +1,14 @@
-import type { Page } from 'playwright';
+import type { Page, Locator } from 'playwright';
 import { isVisible } from '../framework/commonactions';
+import {expect} from "@playwright/test";
 
 export class HomePage {
     readonly page: Page;
+    readonly popUp: Locator;
 
     constructor(page: Page) {
         this.page = page;
+        this.popUp = page.getByTitle('We’ll email you a login link for a password-free sign in.');
     }
 
     async open() {
@@ -18,6 +21,7 @@ export class HomePage {
 
     async goToLoginPage() {
         await this.page.click('css=.google-button');
+        await expect(this.popUp).toBeVisible();
     }
 
 }
--- pages/loginpage.ts
+++ pages/loginpage.ts
@@ -1,23 +1,33 @@
 import type { Page, Locator } from 'playwright';
+import {expect} from "@playwright/test";
 
 export class LoginPage {
     readonly page: Page;
     readonly googleLogin: Locator;
+    readonly googlePage: Locator;
     readonly twitchLogin: Locator;
+    readonly twitchPage: Locator;
     readonly githubLogin: Locator;
+    readonly githubPage: Locator;
     readonly discordLogin: Locator;
+    readonly discordPage: Locator;
 
     constructor(page: Page){
         this.page = page;
         this.googleLogin = page.getByAltText('google logo');
+        this.googlePage = page.getByTitle('Sign in with Google');
         this.twitchLogin = page.getByAltText('twitch logo');
         this.githubLogin = page.getByAltText('github logo');
         this.discordLogin = page.getByAltText('discord logo');
     }
 
-    async userGoogleLogin() {
+    async userGoogleLogin(email:string, password:string) {
         await this.googleLogin.click();
-        await this.googleLogin.type('')
-
+        await expect(this.googlePage).toBeVisible();
+        await this.googlePage.locator('#email').fill(email);
+        await this.googlePage.getByText('Next').click();
+        await this.googlePage.locator('#password').fill(password);
+        await this.googlePage.getByText('Next').click();
     }
+
 }
